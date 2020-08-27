6bb9548c87d04c8e23765b9c4746626108bf261e
Zheng Yi <yi.zheng@moonx.ai>
2020-08-27 18:17:17 +0800

    delete detect dataloader

--- detect.py
+++ detect.py
@@ -1,17 +1,20 @@
 import argparse
 import os
-import platform
-import shutil
+# import platform
+# import shutil
 import time
-from pathlib import Path
+# from pathlib import Path
 
+import math 
+import glob
 import cv2
 import torch
-import torch.backends.cudnn as cudnn
-from numpy import random
+# import torch.backends.cudnn as cudnn
+import numpy as np
 
-from utils.datasets import LoadImages
-from utils.general import (check_img_size, non_max_suppression, 
+
+from utils.datasets import letterbox
+from utils.general import (non_max_suppression, 
             scale_coords, plot_one_box,  set_logging)
 from utils.torch_utils import time_synchronized
 
@@ -19,64 +22,65 @@ from utils.torch_utils import time_synchronized
 def detect(save_img=False):
     source, weights, imgsz = opt.source, opt.weights,  opt.img_size
 
+    img_root_path = "/home/lance/data/DataSets/quanzhou/cyclist/moto/JPEGImages"
+
     # Initialize
     set_logging()
     device = torch.device('cuda:0')
 
     # Load model
     model = torch.load(weights[0], map_location=device)['model'].float().fuse().eval()
-    # model.half()  # to FP16
 
     names = model.module.names if hasattr(model, 'module') else model.names
-    colors = [[random.randint(0, 255) for _ in range(3)] for _ in range(len(names))]
+    colors = [[np.random.randint(0, 255) for _ in range(3)] for _ in range(len(names))]
     
-    # Set Dataloader
-    vid_path, vid_writer = None, None
-    imgsz = check_img_size(imgsz, s=model.stride.max())
-    dataset = LoadImages(source, img_size=imgsz)
+    max_stride = int(model.stride.max())
+    imgsz = math.ceil(opt.img_size / max_stride) * max_stride
 
     # warm up
     img = torch.zeros((1, 3, imgsz, imgsz), device=device)
-    # _ = model(img.half())
     _ = model(img)
     
     t0 = time.time()
-    for path, img, im0s, _ in dataset:
-        img = torch.from_numpy(img).to(device)
-        img = img.float()
-        img /= 255.0  # 0 - 255 to 0.0 - 1.0
+    img_lists = sorted(glob.glob(img_root_path + '/*.jpg'))
+    for img_path in img_lists:
+        
+        img0 = cv2.imread(img_path)
+        # Padded resize
+        img = letterbox(img0, new_shape=imgsz)[0]
+
+        img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
+        img = np.ascontiguousarray(img)
+
+        img = torch.from_numpy(img).to(device).float()
+        img /= 255.0
         
         if img.ndimension() == 3:
             img = img.unsqueeze(0)
 
         # Inference
         t1 = time_synchronized()
+        # pre.shape --> 1*N*(num_cls+1+4), 其中N为 w/(8,16,32) * h/(8,16,32)总和
         pred = model(img)[0]
-        # pre.shape 1*N*(num_cls+1+4), 其中N为 w/(8,16,32) * h/(8,16,32)总和
-
-        t2 = time_synchronized()
-
         # pred [x1, y1, x2, y2, score, cls]
         pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
-        
-        print ("Image size: ", img.shape[2:])
+        t2 = time_synchronized()
         print(" Infer time: {:.3f}".format(t2-t1))
 
         # Process detections
         for i, det in enumerate(pred):  
             
             # normalization gain whwh
-            gn = torch.tensor(im0s.shape)[[1, 0, 1, 0]]
+            gn = torch.tensor(img0.shape)[[1, 0, 1, 0]]
             if det is not None and len(det):
-                # Rescale boxes from img_size to im0 size
-                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0s.shape).round()
+                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], img0.shape).round()
 
                 # draw results
                 for *xyxy, conf, cls in reversed(det):
                     label = '%s %.2f' % (names[int(cls)], conf)
-                    plot_one_box(xyxy, im0s, label=label, color=colors[int(cls)], line_thickness=3)
+                    plot_one_box(xyxy, img0, label=label, color=colors[int(cls)], line_thickness=3)
 
-            cv2.imshow("test", im0s)
+            cv2.imshow("test", img0)
             if cv2.waitKey(0) == ord('q'):  # q to quit
                 raise StopIteration
 
